3d05c95c603625ce576cf6ccc979d1c48b0bd9ad
SoyAlejo <67810567+SoyAlejo@users.noreply.github.com>
2020-08-17 20:24:37 -0500

    Js

--- js/Form.js
+++ js/Form.js
@@ -1,6 +1,6 @@
 (function () {
-	var formulario = document.form_regis;
-	elementos = form_regis;
+	var formulario = document.form;
+	elementos = form;
 
 	// functions
 	var enviar = function (e) {
@@ -20,17 +20,21 @@
 	};
 
 	//Funtion blur y focus
- var focusInput = function() {
-    this.parentElement.children[1].className = "label active";
-    this.parentElement.children[0].className = this.parentElement.children[0].className.replace("error", "");
-  };
+	var focusInput = function () {
+		this.parentElement.children[0].className = "label active";
+		this.parentElement.children[1].className = this.parentElement.children[0].className.replace(
+			" error",
+			""
+		);
+	};
 
-  var blurInput = function() {
-    if (this.value <= 0) {
-      this.parentElement.children[1].className = "label";
-      this.parentElement.children[0].className = this.parentElement.children[0].className + " error";
-    }
-  };
+	var blurInput = function () {
+		if (this.value <= 0) {
+			this.parentElement.children[0].className = "label";
+			this.parentElement.children[1].className =
+				this.parentElement.children[1].className + " error";
+		}
+	};
 
 	// eventos
 	formulario.addEventtListener("submit", enviar);
@@ -45,7 +49,17 @@
 			elementos[i].addEventtListener("Blur", BlurInput);
 		}
     }
-    alert{
-        "Hello"
+     if (elementos.contraseña.value !== elementos.contraseña2.value) {
+      elementos.contraseña.value = "";
+      elementos.contraseña2.value = "";
+      elementos.contraseña.className = elementos.contraseña.className + " error";
+      elementos.contraseña2.className = elementos.contraseña2.className + " error";
+    } else {
+      elementos.contraseña.className = elementos.contraseña.className.replace(" error", "");
+      elementos.contraseña.className = elementos.contraseña2.className.replace(" error", "");
     }
-})();
+
+    return true;
+  };
+
+});
