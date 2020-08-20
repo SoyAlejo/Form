5cb2d06c457850191e7cf8484f361384b6bcc3ea
SoyAlejo <67810567+SoyAlejo@users.noreply.github.com>
2020-08-19 22:27:07 -0500

    js

--- js/Form.js
+++ js/Form.js
@@ -1,24 +1,13 @@
 function validar() {
-	var nombre, apellidos, telefono, correo, pass, pass2;
+	var telefono = document.getElementById("Telefono").value;
+	var pass = document.getElementById("pass").value;
+	var pass2 = document.getElementById("pass2").value;
 
-	Nombre = document.getElementById("Nombre").value;
-	Apellido = document.getElementById("Apellidos").value;
-	Telefono = document.getElementById("Telefono").value;
-	correo = document.getElementById("correo").value;
-	pass = document.getElementById("pass").value;
-	pass2 = document.getElementById("pass2").value;
-
-	if (Telefono.length > 30) {
-		alert("El Telefono es muy largo");
-		return false;
-	} else if (isNaN(Telefono)) {
-		alert("El telefono ingresado no es un numero");
-		return false;
-	}
 	if (pass != pass2) {
 		alert("Las passwords deben de coincidir");
 		return false;
 	} else {
+		alert("");
 		return true;
 	}
 }
